ebeb5a9b7f60ab5e957d0a7916345d3130a18d7f
JoaoPipous <joaodr_340@outlook.com>
2025-06-06 12:35:09 -0300

    Main adicioando mais detalhes

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         Empresa empresa = new Empresa();
 
         empresa.getCaixa().exibirProdutos();
 
-        while(true) {
+        while (true) {
 
             System.out.println("Escolha uma opção:");
             System.out.println("1 - Adicionar funcionário");
@@ -20,11 +19,12 @@ public class Main {
             System.out.println("7 - Listar compras");
             System.out.println("8 - Listar vendas");
             System.out.println("9 - Exibir transportadoras");
-            System.out.println("10 - Mostrar o valor total do caixa da empresa e estimativa de lucros");
-            System.out.println("11 - Exibir negócios em aberto");
-            System.out.println("12 - Sair");
+            System.out.println("10 - Setar Plano de Saude e Odontologico");
+            System.out.println("11 - Mostrar o valor total do caixa da empresa e estimativa de lucros");
+            System.out.println("12 - Exibir negócios em aberto");
+            System.out.println("13 - Sair");
 
-            int opcao = Integer.parseInt(sc.nextLine());
+            int opcao = sc.nextInt();
 
             switch (opcao) {
 
@@ -65,14 +65,44 @@ public class Main {
                 }
 
                 case 10: {
-                    break;
+                    int planos;
+                     do{
+                        System.out.println("\n--- PLANOS DE VIDA ---");
+                        System.out.println("1 - Informar gastos com plano de saude e odontologico");
+                        System.out.println("0 - Voltar ao menu principal");
+                        System.out.print("Escolha: ");
+                        planos = sc.nextInt();
+
+                        switch (planos) {
+                            case 1:
+                                System.out.print("Digite o valor gasto com plano de saude: ");
+                                double planoSaude = sc.nextDouble();
+                                System.out.print("Digite o valor gasto com plano odontologico: ");
+                                double planoOdonto = sc.nextDouble();
+
+
+                            case 0:
+                                System.out.println("Voltando ao menu principal...");
+                                break;
+
+                            default:
+                                System.out.println("Opção inválida.");
+                                break;
+                        }
+                    }while(planos != 0);
                 }
 
-                case 11: {
+
+                case 11:{
                     break;
                 }
 
                 case 12: {
+                    break;
+                }
+
+                case 13 : {
+                    System.out.printf("\n--- SAINDO DO PROGRAMA ATÉ BREVE---\n");
                     sc.close();
                     return;
                 }
